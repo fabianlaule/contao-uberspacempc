826909949e714c9bc7bc39c6148774873e386a49
Fabian Laule <github@fabianlaule.de>
2014-02-19 23:42:18 +0100

    Add a field for redirecting the user after a successful password change

---
+++ languages/de/tl_module.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   UberspaceMPC
+ * @author    Fabian Laule <fabianlaule.de>
+ * @license   LGPL
+ * @copyright Fabian Laule 2013
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['uberspaceMPCredirect']   = array('Weiterleiten nach erfolgreichem Passwortwechsel', 'Nach erfolgreichem Passwortwechsel auf eine andere Seite wechseln.');
---
+++ languages/en/tl_module.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   UberspaceMPC
+ * @author    Fabian Laule <fabianlaule.de>
+ * @license   LGPL
+ * @copyright Fabian Laule 2013
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['uberspaceMPCredirect']   = array('Redirect to another page after submitting the form', 'Here you can redirect the user to another page if the password change was successful.');
